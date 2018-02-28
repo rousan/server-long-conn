024d196d58646022f644c20a46c53f9a2fd3d0be
Rousan Ali <rousanali786@gmail.com>
2018-02-28 18:58:47 +0530

    Add app.js

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # server-long-conn
+
 The strange server implementation whose full response can't be accessed at once
---
+++ src/app.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+const http = require('http');
+
+const contentText = fs.readFileSync(path.join(__dirname, 'index.html'), { encoding: 'utf8' });
+const interval = 0;
+
+http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+
+  let i = 0;
+  const ln = contentText.length;
+
+  setTimeout(function fn() {
+    res.write(contentText.charAt(i));
+    i += 1;
+    if (i >= ln) {
+      return;
+    }
+    setTimeout(fn, interval);
+  }, interval);
+}).listen(8080);
